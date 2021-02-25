250f2e67f832017c7925df637e4d6ad5a722cba1
Thiago Kachinsky <tjk091@gmail.com>
2021-02-25 11:11:27 -0300

    Dia 04 - NLW - Melhorando a usabilidade do app

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/ThiagoKachi.png" alt="Thiago Kachinsky"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Thiago Kachinsky</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
